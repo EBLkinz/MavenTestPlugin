936392d296f5f5e3ac0b7baf56cf81eae234ad62
BuildTools <unconfigured@null.spigotmc.org>
2018-05-17 13:04:42 -0500

    Initial check-in.

---
+++ src/main/java/io/github/eblkinz/mavenplugintest/MavenPluginTest.java
@@ -0,0 +1,18 @@
+package io.github.eblkinz.mavenplugintest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MavenPluginTest extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		//TODO
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		//TODO
+	}
+}
